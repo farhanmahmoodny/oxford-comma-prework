ab21cb7030edefc75aca5deb8617424b6d03fe1f
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-13 04:34:15 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,6 @@ def oxford_comma(array)
     array.join(" and ")
   elsif array.length == 3
     array.insert(-2, "and")
-    array.join(", ")
-    array.push(array.pop)
+    array.join(",")
   end
 end
