dc6da55102367fe04c1525edfb11ddc33cac2897
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-13 04:45:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,12 +3,7 @@ def oxford_comma(array)
     array.join
   elsif array.length == 2
     array.join(" and ")
-  # elsif array.length == 3
-  # array[0] = array[0] + ","
-  # array[1] = array[1] + ","
-  # array.insert(-2, "and")
-  # array.join(" ")
-  else
+ else
     count = 0
     until count == array.length-1
     array[count] = array[count] + ","
