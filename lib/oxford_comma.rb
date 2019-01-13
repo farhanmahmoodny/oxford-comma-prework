ce10d786a9add19f456dcc6c05aa8fe9d7da1d44
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-13 04:41:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,10 +4,6 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   elsif array.length == 3
-    # array.insert(-2, "and")
-    # array.insert(1, ",")
-    # array.insert(3, ",")
-    # array.join("")
   array[0] = array[0] + ","
   array[1] = array[1] + ","
   array.insert(-2, "and")
